8e8576e72828abb113ed9400ee3722a80dc77bf1
cbudel <cbudel@celepar.pr.gov.br>
2019-09-04 15:19:46 -0300

    Created a service

---
+++ src/main/java/br/com/apptest/api/NotaService.java
@@ -0,0 +1,41 @@
+package br.com.apptest.api;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+
+/**
+ * Classe provedora de serviços rest de Nota. 
+ * @author GIC
+ * @since 1.0 
+ * @version 1.0 - 17/04/19
+ */
+@Path("v1/nota")
+public class NotaService {
+	
+	@Inject
+	private Logger log;
+			
+	/**
+	 * Retorna as notas do aluno por sua matrícula.
+	 * @param idAluno
+	 * @return
+	 */
+	@Path("/{id_aluno}")
+	@GET
+    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	public Response listarPorMatricula(@PathParam("id_aluno") Integer idAluno) {
+		
+		log.debug("NotaService Acessed - Aluno:" + idAluno);
+		return Response.ok().entity(new String("Nota do aluno:" + idAluno)).build();
+		
+	}
+
+	
+}
