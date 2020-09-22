cdbf03e9e8e93ac5a655b286c593c368af964f96
Cleverson Budel <54901789+cleversonb@users.noreply.github.com>
2020-09-22 09:06:30 -0300

    Update SLF4JProducer.java

--- src/main/java/br/com/apptest/util/SLF4JProducer.java
+++ src/main/java/br/com/apptest/util/SLF4JProducer.java
@@ -1,19 +1,14 @@
 package br.com.apptest.util;
 
 import java.io.Serializable;
-
 import javax.enterprise.inject.Produces; 
 import javax.enterprise.inject.spi.InjectionPoint;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Classe responsável por definir a forma de instanciação do Logger para o framework SLF4J.
- * Isto possibilita o uso de injeção (@Inject) para instanciação do log. 
- * @author celepar - gic
- * @version 1.0, 27/12/2013
- * @since 1.0
+ * Isto possibilita o uso de injeção (@Inject) para instanciação do log.
  */
 public class SLF4JProducer implements Serializable {
 
